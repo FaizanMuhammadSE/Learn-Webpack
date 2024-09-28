9849cb50c05498466012d5a0030de64ef10a23b7
FaizanMuhammadSE <jutt41697@gmail.com>
2024-09-28 15:44:35 +0500

    Asset Types Explained - asset/resource - asset/inline - asset/source - asset

--- 3-Assets/src/addImage.js
+++ 3-Assets/src/addImage.js
@@ -1,8 +1,10 @@
 import medicine from './medicine.jpg';
+import altText from './imgText.txt';
 
 export const addImage = () => {
   const img = document.createElement('img');
   img.src = medicine;
+  img.alt = altText;
   const body = document.querySelector('body');
   debugger;
   body.appendChild(img);
--- 3-Assets/webpack.config.js
+++ 3-Assets/webpack.config.js
@@ -12,6 +12,16 @@ module.exports = {
         test: /\.(png|jpg|jpeg|gif|svg)$/i,
         type: 'asset/resource', // It will create Separete file for these assets, and will be served over network
       },
+      // {
+      //   test: /\.(png|jpg|jpeg|gif|svg)$/i,
+      //   type: 'asset/inline', // It will embed the asset in the bundle itself in Base64-format, consequence is bundle size will increase
+      // },
+      {
+        test: /\.(txt)$/i,
+        type: 'asset/source', // It will embed the asset in the bundle itself
+      },
+      // If we will set type to 'asset' then Webpack will automatically decide whether to embed or create separate file based on the size of the asset
+      // So if size is less than 8kb then it will embed (asset/inline) the asset in the bundle itself in Base64Format, otherwise it will create separate file (asset/resource)
     ],
   },
   mode: 'development',
